3d2a8827563a100675c3b9badab31601d204d0a2
Gentle Shark <40314351+ShaemaSigle@users.noreply.github.com>
2022-09-13 23:02:35 +0300

    Add files via upload
    Something I am tired of writing again and again for every new project

---
+++ ClockForUnityGames.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Overlord : MonoBehaviour
+{
+    //1 minute IN = 1 second OUT
+    //1 hour IN = 1 minute OUT
+    //Text Mesh Pro is needed if you don't want to rewrite anything
+    public TextMeshProUGUI timeText;
+    public int hours = 0;
+    public int minutes = 0;
+    public int day = 1;
+    public string strHours = "0"; 
+    public string strMinutes = "0";
+
+    void Update()
+    {
+        #region TimeCalculator
+        minutes = (int)Time.time - ((60 * hours) + ((day-1) * 24 * 60)) ;
+       if(minutes==60){
+        hours++;
+       }
+       if(hours == 24){
+        day++;
+        hours = 0;
+       }
+       if(hours<=9){
+        strHours = "0" + hours;
+       }else{
+        strHours = hours.ToString();
+       }
+       if(minutes<=9){
+        strMinutes = "0" + minutes;
+       }else{
+        strMinutes = minutes.ToString();
+       }
+       timeText.text = "Day " + day + " " + strHours + ":" + strMinutes;
+       #endregion 
+    }
+}
