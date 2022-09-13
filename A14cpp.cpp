785b256b4b841028a9f249fd26e49205f38b3fd4
ShaemaSigle <40314351+ShaemaSigle@users.noreply.github.com>
2022-09-13 22:49:25 +0300

    Add files via upload

---
+++ A14cpp.cpp
@@ -0,0 +1,50 @@
+/*
+Jūlija Tatarinceva, jt22005
+A14. Dots naturāls skaitlis n. Noteikt, vai n ir pirmskaitlis.
+Programma izveidota: 2022/09/11
+*/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, redo;
+    do{
+        bool isPrime = true;
+        cout << "Please enter a natural number \n";
+        cin >> n;
+        if(n<2){
+            cout << "Prime numbers are greater that 1 \n";
+        }
+        else{
+            for (int i = 3; i < n; i++){
+                if((n%i) == 0){
+                    isPrime = false;
+                    break;
+                }
+            }
+            if(isPrime){
+                cout << n << " is prime \n";
+            }else{
+                cout << n << " is not considered prime \n";
+            }
+        }
+        cout << "Would you like to continue(1) or stop(any key) testing? \n";
+        cin >> redo;
+    } while(redo == 1);
+}
+/*              Testa plāns
+ ____________________________________________
+ |Ievads|  Rezultāts |Vai pareizs rezultāts?|
+ -------|------------|----------------------|
+ |  1   |nav pirmsk. |          +           |
+ -------|------------|----------------------|
+ |  7   |ir pirmsk   |          +           |
+ -------|------------|----------------------|
+ |  8   |nav pirmsk. |          +           |
+ |------|------------|----------------------|
+ |  -5  |nav pirmsk. |          +           |
+ ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯
+ */
+
