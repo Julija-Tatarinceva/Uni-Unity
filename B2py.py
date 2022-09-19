8cac9fb4dca0b43de215c9fd984cd2a2ac9d2342
Gentle Shark <40314351+ShaemaSigle@users.noreply.github.com>
2022-09-19 21:25:04 +0300

    Update B2py.py

--- B2py.py
+++ B2py.py
@@ -1,19 +1,18 @@
 #  Jūlija Tatarinceva, jt22005
 #  B2. Dots naturāls skaitlis n < 1000. Izdrukāt doto skaitli romiešu pierakstā.
 #  Programma izveidota: 2022/09/13
-romanNumber: str = "" #this will be filled with roman symbols during conversion
+romanNumber: str = "" #this will be filled with roman symbols
 RomanSymbols = ["CDM", "XLC", "IVX"] #first str is used to convert hundreds, second - dozens and the last one - units
-usePack: str = ""
 
 def Converter(numToConvert, symbolsUsed):
-    romanPart: str = "" #it needs to be cleared each time the function is called since each time it is working with a new number
+    romanPart: str = "" #needs to be cleared each time the function is called
     if numToConvert == "5":
         romanPart = symbolsUsed[1]
     elif numToConvert == "4":
         romanPart = symbolsUsed[0] + symbolsUsed[1]
     elif numToConvert == "9":
         romanPart = symbolsUsed[0] + symbolsUsed[2]
-    elif int(numToConvert) < 4:
+    elif int(numToConvert) < 5:
         for i in range(1, int(numToConvert)+1):
             romanPart = romanPart + symbolsUsed[0]
     elif (int(numToConvert) > 5):
@@ -26,13 +25,12 @@ while True:
         romanNumber = ""
         while True: #protection against logically incorrect input
             n = int(input("Please enter a natural number \n"))
-            if n > 0: 
+            if n > 0 and n < 1000: 
                 break
             else:
-                print ("Numbers that are ≤ 0 can not be converted")
-                continue
+                print ("Can not be converted")
         numberOfChars = len(str(n))
-        for i in range(0, numberOfChars): #converter is called 1 to 3 times to convert every symbol seperately
+        for i in range(0, numberOfChars): #converter is called to convert every symbol separately
             if numberOfChars == 2:
                 useSymbols = RomanSymbols[i+1] #skipping hundreds
             elif numberOfChars == 1:
