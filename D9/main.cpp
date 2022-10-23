85833f2e92ca8d2f4c37f28f7e5570913ed246eb
Gentle Shark <40314351+ShaemaSigle@users.noreply.github.com>
2022-10-23 16:30:03 +0300

    Update main.cpp

--- D9/main.cpp
+++ D9/main.cpp
@@ -1,3 +1,11 @@
+/*Jūlija Tatarinceva, jt22005
+D9. "Izveidot klasi ""Skaitļu steks"" - Nstack, kurā glabājas masīvs ar skaitliskām vērtībām (int) garumā 5 un steka elementu skaits. Klasei izveidot šādas metodes: 
+(1) konstruktors, (2) destruktors, kurš paziņo par objekta likvidēšanu un likvidēto elementu skaitu (ja likvidēšanas brīdī steks nav tukšs), (3) metode ""Ielikt"" - push, kas pievieno stekam elementu, ja tas nav pilns, 
+(4) metode ""Izņemt"" - pop, kas izņem no steka augšējo elementu un atgriež tā vērtību, (5) metode ""CikElementu"" - count, kas atgriež elementu skaitu stekā, 
+(6) metode ""Augša"" - top, kas atgriež augšējā elementa vērtību, (7) metode ""VaiTukšs"" - isEmpty, kas noskaidro, vai steks ir tukšs, 
+(8) metode ""VaiPilns"" - isFull, kas noskaidro, vai steks ir pilns.  
+Programma izveidota: 2022/10/23 */
+
 #include <iostream>
 #include <stack>
 #include "program.h"
@@ -18,4 +26,15 @@ int main() {
     else if(exec==8) obj.isFull();
     else break;
     }while(true);
-}
+}/*              Testa plāns
+ ____________________________________________
+ |Ievads|  Rezultāts |Vai pareizs rezultāts?|
+ -------|------------|----------------------|
+ |      |            |                      |
+ -------|------------|----------------------|
+ |      |            |                      |
+ -------|------------|----------------------|
+ |      |            |                      |
+ |------|------------|----------------------|
+ |      |            |                      |
+ ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯*/
