685617d6086bf7ea9d8aeb20a00519c51201ab5f
Gentle Shark <40314351+ShaemaSigle@users.noreply.github.com>
2022-12-22 11:54:48 +0200

    Update ClockForUnityGames_Template.cs

--- ClockForUnityGames_Template.cs
+++ ClockForUnityGames_Template.cs
@@ -7,6 +7,7 @@ public class ClockForUnityGames_Template.cs : MonoBehaviour
 {
     //1 minute IN = 1 second OUT
     //1 hour IN = 1 minute OUT
+    //It doesn't really matter (except for the days), just change the names of the variables if needed
     //Text Mesh Pro is needed if you don't want to rewrite anything
     public TextMeshProUGUI timeText;
     public int hours = 0;
@@ -21,15 +22,15 @@ public class ClockForUnityGames_Template.cs : MonoBehaviour
         minutes = (int)Time.time - ((60 * hours) + ((day-1) * 24 * 60)) ;
        if(minutes==60){
         hours++;
+        if(hours<=9){
+         strHours = "0" + hours;
+        }else{
+         strHours = hours.ToString();
+        }
        }
        if(hours == 24){
         day++;
         hours = 0;
-       }
-       if(hours<=9){
-        strHours = "0" + hours;
-       }else{
-        strHours = hours.ToString();
        }
        if(minutes<=9){
         strMinutes = "0" + minutes;
