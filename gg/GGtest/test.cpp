a5507dbe85558d268453c63f1ccdba997e9f6701
Gentle Shark <40314351+ShaemaSigle@users.noreply.github.com>
2023-06-01 11:52:18 +0300

    Update test.cpp
    Corrected test C

--- gg/GGtest/test.cpp
+++ gg/GGtest/test.cpp
@@ -1,3 +1,8 @@
+/* Jūlija Tatarinceva, jt22005
+G19. Uzrakstīt funkciju, kas no saraksta izmet tos elementus, 
+tieši aiz kuriem sarakstā seko elementi, kuru vērtība ir pāra skaitlis.
+Programma izveidota: 2023/03/02 */
+
 #include <iostream>
 #include <list>
 #include <time.h>
@@ -6,44 +11,39 @@
 using namespace std;
 #include "../GGmain/G19-vienvirziena_saistits_saraksts.cpp"
 
-//pārbauda, vai funkcija nostrādās ar tukšo sarakstu
-void test_A() {
+//Tests A - tukšs saraksts
+bool test_A() {
     elem* test = NULL;
     try {
         RemoveBeforeEvenElements(test, test);
     } catch (int issue) {
-        if (issue != -1) {
-            cout << "Test_A failed (tukss saraksts)." << endl;
-        } else {
-            cout << "Test_A passed (tukss saraksts)." << endl;
-        }
+        if (issue == -1) return true;
     }
+    return false;
 }
 
-//tests ar vienu elementu sarakstā, tiek parbaudīts, vai elements izmainās
-void test_B() {
-    elem* last;
-    elem* first;
-    elem* test = new elem;
+//Tests B - saraksts ar vienu elementu
+bool test_B() {
+    elem* last; elem* first; elem* test = new elem;
     test->num = 1;
     test->next = NULL;
     first=last=test;
     RemoveBeforeEvenElements(test, test);
-    if(first->num==1 && first->next == NULL) cout << "Test_B passed (1 elements sarakstaa)." << endl;
-    else cout << "Test_B failed (1 elements sarakstaa)." << endl;
+    if(first->num==1 && first->next == NULL) return true; //tiek parbaudīts, vai elements izmainās
+    else return false;
 }
 
-//tests ar sarakstu no 6 elementiem, tiek parbaudīts, vai funkcija pareizi izmet elementus, tieši aiz kuriem seko pāra skaitlis
-void test_C() {
-    int test_input[6] = {2, 3, 4, 6, 7, 9};
-    int test_output[4] = {2, 6, 7, 9};
+//Tests C - saraksts ar vairākiem elementiem
+bool test_C() {
+    const int test_array_length = 6;
+    const int result_array_length = 4;
+    int test_input[test_array_length] = {2, 3, 4, 6, 7, 9}; //saraksts sākumā
+    int test_output[result_array_length] = {2, 6, 7, 9}; //sagaidāms rezultāts
 
-    elem* last;
-    elem* first;
-    elem* test = new elem;
+    elem* last; elem* first; elem* test = new elem;
     test->num = test_input[0];
     first=last=test;
-    for(int i = 1; i<6; i++){
+    for(int i = 1; i<test_array_length; i++){
         elem* p = new elem;
         p->num = test_input[i];
         p->next=NULL;
@@ -53,23 +53,22 @@ void test_C() {
 
     RemoveBeforeEvenElements(test, test);
 
-    int j=0; bool err = false;
-    elem* i;
-    for(i=first; i; i=i->next){
-        if (i->num != test_output[j]){
-            cout << "Test_C failed (vairaaki elementi sarakstaa)." << endl;
-            err = true;
-            break;
-        }
+    int j=0; elem* i; //skaitītāji ciklam
+    for(i=first; i; i=i->next){  //tiek parbaudīts, vai rezultātā tiek izveidots pareizs saraksts
+        if(j == result_array_length) return false;
+        if (i->num != test_output[j]) return false;
         else j++;
     }
-    if (!err) cout << "Test_C passed (vairaaki elementi sarakstaa)." << endl;
+    return true;
 }
 
 int main(){
     cout << "Ruupiigi testeejam..."<<endl;
-    test_A();
-    test_B();
-    test_C();
+    if (!test_A()) cout << "Test_A failed." << endl;
+    else cout << "Test_A passed." << endl;
+    if (!test_B()) cout << "Test_B failed." << endl;
+    else cout << "Test_B passed." << endl;
+    if (!test_C()) cout << "Test_C failed." << endl;
+    else cout << "Test_C passed." << endl;    test_A();
     cout << "Testeesana pabeigta!"<<endl;   
 }
