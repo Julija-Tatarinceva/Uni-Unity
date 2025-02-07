d27fc634c8d3a7943397a2b89d6b0f97223c0765
Julija Tatarinceva <40314351+Julija-Tatarinceva@users.noreply.github.com>
2025-02-07 11:34:52 +0200

    Update ClockForUnity_v2.cs
    Removed unnecessary variables and fixed weird text results (0:00)

--- ClockForUnity_v2.cs
+++ ClockForUnity_v2.cs
@@ -3,40 +3,38 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
-public class Overlord : MonoBehaviour
-{
-    public TextMeshProUGUI ScoreText;
+public class TimeCalculator : MonoBehaviour{
     public TextMeshProUGUI timeText;
-    public int minutes = 0;
-    public int seconds = 0;
-    string strMinutes = "0"; 
-    string strSeconds = "0";
-    public float TimePassed = 0;
-    public int score = 0;
+    private int _minutes = 0;
+    private int _seconds = 0;
+    string _strMinutes = "0"; 
+    string _strSeconds = "0";
+    public float timePassed = 0;
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start(){
         minutes = 0;
         seconds = 0;
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update(){
         #region TimeCalculator
-        TimePassed += Time.deltaTime;
-        seconds = (int)TimePassed - (60 * minutes);
-        if(minutes==60){
-            minutes++;
-            if(minutes<=9) strMinutes = "0" + minutes;
-            else strMinutes = minutes.ToString();
-        }
-       if(seconds<=9) strSeconds = "0" + seconds;
-       else strSeconds = seconds.ToString();
-       timeText.text = strMinutes + ":" + strSeconds;
+        timePassed += Time.deltaTime; // Fetching time that has passed since the last frame and adding it to the sum
+            // Using math to output the time in seconds using minutes:seconds format
+            _seconds = (int)timePassed - (60 * _minutes);
+            if(_seconds==60) {
+                // Adding another minute to the timer
+                _minutes++;
+                _strMinutes = _minutes switch {
+                    >= 9 => _minutes.ToString(),
+                    < 9 => "0" + _minutes
+                };
+            }
+            _strSeconds = _seconds switch {
+                <=9 => "0" + _seconds,
+                _ => _seconds.ToString()
+            };
+            timeText.text = _strMinutes + ":" + _strSeconds;
        #endregion
-    }
-    public void UpdateGUI(){
-        ScoreText.text = "Score:" + score.ToString();
     }
 }
